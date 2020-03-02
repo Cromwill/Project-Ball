cb5570ed40000306e2923f8028a9de0d1af8dd45
Cromwill <33699699+Cromwill@users.noreply.github.com>
2020-03-02 14:03:32 +0300

    Create Slowndone

---
+++ Assets/Scriptes/ActionObjects/Slowdone.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class Slowdone : MonoBehaviour
+{
+    [SerializeField] private Vector2 _maxSpeed;
+    [SerializeField] private Direction _direction;
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        Debug.Log(collision.name);
+        Rigidbody2D rigidbody = collision.GetComponent<Rigidbody2D>();
+        rigidbody.velocity = RetentionSpeed(rigidbody.velocity);
+    }
+
+    private Vector2 RetentionSpeed(Vector2 speed)
+    {
+        return _direction == Direction.Horizontal ? MoveHorizontal() : MoveVertical(speed);
+    }
+
+    private Vector2 MoveVertical(Vector2 speed)
+    {
+        if (Mathf.Abs(speed.x) > _maxSpeed.x)
+            return new Vector2(_maxSpeed.x * Mathf.Sign(speed.x), speed.y);
+
+        if (Mathf.Abs(speed.y) > _maxSpeed.y)
+            return new Vector2(speed.x, _maxSpeed.y * Mathf.Sign(speed.y));
+        return speed;
+    }
+
+    private Vector2 MoveHorizontal()
+    {
+        return _maxSpeed;
+    }
+
+    public enum Direction
+    {
+        Vertical,
+        Horizontal
+    }
+}
